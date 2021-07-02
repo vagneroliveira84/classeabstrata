213d5352f0fc2771ced97a0384e0240085e325d0
Vagner Oliveira <vagner.natalicio@gmail.com>
2021-07-02 08:18:05 -0300

    Atualizando Classe Account

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,5 +1,48 @@
 package entities;
 
 public class Account {
+	
+	public Integer number;
+	public String holder;
+	public Double balance;
+	
+	public Account() {
+		
+	}
 
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	
+	public void withdraw(double amount) {
+		balance -=amount + 5.0;
+	}
+	
+	public void deposit(double amount) {
+		balance +=amount;
+	}
 }
+
+
